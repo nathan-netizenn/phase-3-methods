fa5c0ea2262424cdadfc3a8a213faf5eb9167c8a
nathan <nathanmageka6@gmail.com>
2023-05-19 14:10:19 +0300

    nathan

--- methods.rb
+++ methods.rb
@@ -1,1 +1,16 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+   end
+   def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  def add(num1, num2)
+    num1 + num2
+  end
+  def halve(number)
+    return number / 2 if number.is_a?(Integer)
+    return nil
+  end
